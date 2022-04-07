86f08b45c196b007ce8af7cdabb69081a9e0bdee
rwang4 <rwang4@ualberta.ca>
2022-04-07 12:46:18 -0700

    acitivies+search+profile pages inited

---
+++ lib/pages/activity/activity_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ActivityPage extends StatelessWidget {
+  const ActivityPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Activity Page'),
+      ),
+      body: const Center(
+        child: Text('Activity Page'),
+      ),
+    );
+  }
+}
--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,6 +1,8 @@
 export 'app.dart';
+export 'main_page.dart';
 export 'home/home_page.dart';
 export 'signup/signup_page.dart';
 export 'login/login_page.dart';
 export 'login/email_password_page.dart';
+export 'profile/profile_page.dart';
 export 'widgets/widgets.dart';
